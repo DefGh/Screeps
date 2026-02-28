ba1d648697f74bc69d960c70400f36f2646966fa
Davit Gharagebakyan <defghq@gmail.coom>
2026-02-28 15:06:28 +0400

    feat(Screeps): integrate room, creep, and spawn prototypes
    This commit integrates the `room.prototype`, `creep.prototype`, and `spawn.prototype` modules into the main script of the Screeps AI. This enhancement ensures that all necessary prototype functions are loaded and utilized during each game loop iteration, improving code organization and functionality.

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
 
+require('room.prototype');
+require('creep.prototype');
+require('spawn.prototype');
+ 
+
+module.exports.loop = function () {
+    Memory.wasReplaced = false;
+
+    for (const roomName in Game.rooms) {
+        const room = Game.rooms[roomName]
+        room.run();
+    }
+
+    for (const creepName in Game.creeps) {
+        const creep = Game.creeps[creepName];
+        creep.run();
+    }
+
+    for (const name in Memory.creeps) {
+        if (!Game.creeps[name]) {
+            delete Memory.creeps[name];
+        }
+    }
+}
