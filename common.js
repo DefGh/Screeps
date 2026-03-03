e86cf3f710fc87899d271cec5e4ef98e3356bc60
Davit Gharagebakyan <defghq@gmail.coom>
2026-03-03 20:10:52 +0400

    asd

--- common.js
+++ common.js
@@ -1,3 +1,5 @@
+const constants = require('./constants');
+
 constants = require('constants');
 
 module.exports = {
@@ -8,66 +10,32 @@ module.exports = {
         // get max energy
         let maxEnergy = Game.spawns['Spawn1'].room.energyCapacityAvailable;
         //console.log('Max energy available:', maxEnergy);
-        
+        var pattern = [];
         switch (role) {
             case constants.roles.UNIVERSAL:
-                var aval = maxEnergy;
-                let parts = [];    
-                //console.log('Building universal body with', aval, 'energy');
-                let universal_body = [constants.BodyParts.MOVE, constants.BodyParts.CARRY, constants.BodyParts.WORK]
-                
-                // Для универсального крипа создаем сбалансированное тело
-                // Сначала добавляем по одной части каждого типа для базовой функциональности
-                let baseParts = [constants.BodyParts.MOVE.part, constants.BodyParts.CARRY.part, constants.BodyParts.WORK.part];
-                let baseCost = constants.BodyParts.MOVE.cost + constants.BodyParts.CARRY.cost + constants.BodyParts.WORK.cost;
-                
-                if (aval >= baseCost) {
-                    // Добавляем базовые части
-                    parts = baseParts.slice();
-                    aval -= baseCost;
-                    
-                    // Распределяем оставшуюся энергию пропорционально
-                    while (aval > 0) {
-                        let partsAdded = false;
-                        for (let part of universal_body) {
-                            if (aval >= part.cost) {
-                                aval -= part.cost;
-                                parts.push(part.part);
-                                partsAdded = true;
-                            }
-                        }
-                        // Если не удалось добавить ни одной части, выходим из цикла
-                        if (!partsAdded) {
-                            break;
-                        }
-                    }
-                } else {
-                    // Если энергии не хватает даже на базовые части, добавляем только MOVE
-                    while (aval >= constants.BodyParts.MOVE.cost) {
-                        aval -= constants.BodyParts.MOVE.cost;
-                        parts.push(constants.BodyParts.MOVE.part);
-                    }
-                }
-                
-                //console.log('Final body parts:', parts);
-                return parts;
-                break;
+                pattern = [constants.BodyParts.MOVE, constants.BodyParts.CARRY, constants.BodyParts.WORK, constants.BodyParts.MOVE, constants.BodyParts.CARRY, constants.BodyParts.WORK, constants.BodyParts.MOVE, constants.BodyParts.CARRY, constants.BodyParts.WORK,constants.BodyParts.MOVE, constants.BodyParts.CARRY, constants.BodyParts.WORKconstants.BodyParts.MOVE, constants.BodyParts.CARRY, constants.BodyParts.WORK]  
             case constants.roles.MINER:
-                // Для минера создаем тело с максимальным количеством WORK частей
-                let workParts = Math.min(5, Math.floor(maxEnergy / 200));
-                let minerParts = [];
-                
-                // Добавляем WORK части
-                for (let i = 0; i < workParts; i++) {
-                    minerParts.push(constants.BodyParts.WORK.part);
-                }
-                
-                //console.log('Miner body parts:', minerParts);
-                return minerParts;
-                break;
+                pattern = [constants.BodyParts.WORK, constants.BodyParts.WORK, constants.BodyParts.WORK, constants.BodyParts.WORK, constants.BodyParts.WORK]
             default:
                 //console.log('Unknown role, returning empty body');
                 return [];
         }
+
+        var avail = maxEnergy; 
+        var body = []
+        while (avail > 0) {
+            for(let part of pattern) {
+                if (avail > part.cost)
+                {
+                    avail -= part.cost
+                    body.push(part.part)
+                }   
+                else {
+                    return body;
+                }
+
+            }
+        }
+        return body;
     }
 }
