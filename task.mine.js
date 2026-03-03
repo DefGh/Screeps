753dc576bd748e73d9b779b2b10779fbaab6d86a
Davit Gharagebakyan <defghq@gmail.coom>
2026-03-04 00:32:38 +0400

    йц

--- task.mine.js
+++ task.mine.js
@@ -2,20 +2,33 @@ const constants = require("constants");
 const taskManager = require("task.manager")
 module.exports = {
     run : function(creep, task) {
+        console.log('DEBUG: task.mine.run called for creep:', creep.name);
+        console.log('DEBUG: task.data.sourceId:', task.data.sourceId);
+        console.log('DEBUG: task.data.position:', task.data.position);
+        
         // 1 - init
         let sourceId = task.data.sourceId;
         let source = Game.getObjectById(sourceId);
         let position = task.data.position;
+        
+        console.log('DEBUG: source:', source);
+        console.log('DEBUG: position:', position);
+        console.log('DEBUG: creep.pos:', creep.pos);
+        
         // 2 - if on destination coord - mine
         if (creep.pos.x === position.x && creep.pos.y === position.y) {
+            console.log('DEBUG: creep is on position, trying to harvest');
             // Mine energy from source
             let result = creep.harvest(source);
+            console.log('DEBUG: harvest result:', result);
+            
             if (result === ERR_NOT_ENOUGH_RESOURCES) {
                 // Source is empty, just wait
                 creep.say('waiting');
                 return false; // Task not finished, continue waiting
             } else if (result !== OK) {
                 // Other error, try to move to correct position
+                console.log('DEBUG: harvest error, trying to move to position');
                 creep.moveTo(position);
                 return false; // Task not finished
             } else {
@@ -26,8 +39,9 @@ module.exports = {
         }
         // 3 - else - try create taxi task and move to coord
         else {
+            console.log('DEBUG: creep is not on position, creating taxi task');
             // Create taxi task to move to mining position
-            taskManager.tryAddTask({
+            let taxiTaskResult = taskManager.tryAddTask({
                 type: constants.taskTypes.TAXI,
                 canExecute: [constants.roles.UNIVERSAL],
                 repeatable: false,
@@ -38,6 +52,8 @@ module.exports = {
                     where: position
                 }
             });
+            console.log('DEBUG: taxi task creation result:', taxiTaskResult);
+            
             creep.moveTo(position);
             return false; // Task not finished, still moving
         }
--- task.taxi.js
+++ task.taxi.js
@@ -2,22 +2,36 @@ const constants = require("constants");
 const taskManager = require("task.manager")
 module.exports = {
     run : function(creep, task) {
+        console.log('DEBUG: task.taxi.run called for creep:', creep.name);
+        console.log('DEBUG: task.data.whom:', task.data.whom);
+        console.log('DEBUG: task.data.where:', task.data.where);
+        
         // 1 - init
         let whomId = task.data.whom;
         let where = task.data.where;
         
         // Get the target creep
         let targetCreep = Game.getObjectById(whomId);
+        console.log('DEBUG: targetCreep:', targetCreep);
+        
         if (!targetCreep) {
-                        return true; // Task completed (target died)
+            console.log('DEBUG: targetCreep not found, task completed');
+            return true; // Task completed (target died)
         }
+        
+        console.log('DEBUG: targetCreep.pos:', targetCreep.pos);
+        console.log('DEBUG: where:', where);
+        
         // 2 - if target creep is at destination - task is done
         if (targetCreep.pos.x === where.x && targetCreep.pos.y === where.y) {
+            console.log('DEBUG: target is at destination, task completed');
             // Task completed - target reached destination
             creep.say('✅ Done');
             return true; // Task completed successfully
         }
+        
         // 3 - move to target and escort
+        console.log('DEBUG: moving to target position');
         creep.moveTo(targetCreep.pos);
         return false; // Task not finished yet
     }
