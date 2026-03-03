f7602b9268d7140f241a381cb33e91dfa2a407e2
Davit Gharagebakyan <defghq@gmail.coom>
2026-03-04 00:12:28 +0400

    asd

--- task.spawnCreep.js
+++ task.spawnCreep.js
@@ -3,18 +3,6 @@ const constants = require("constants");
 module.exports = {
 
     run: function (executer, task) {
-        let taskFinished = false;
-
-        // Validate task data
-        if (!task.data || !task.data.role || !task.data.body) {
-            return true; // Task is finished (failed)
-        }
-
-        // Check if executer is a spawn
-        if (!executer || !executer.spawnCreep) {
-            return true; // Task is finished (failed)
-        }
-
         // Check if spawn is currently busy
         if (executer.spawning) {
             return false; // Task remains in progress, will retry next tick
@@ -35,25 +23,7 @@ module.exports = {
             }
         );
 
-
-
-        switch (spawnResult) {
-            case OK:
-            case ERR_INVALID_ARGS:
-            case ERR_NAME_EXISTS:        
-                taskFinished = true;
-                break;
-            case ERR_NOT_ENOUGH_ENERGY:
-                taskFinished = false;
-            default:
-               taskFinished = false;
-        }
-
-        if (taskFinished)
-            console.log(spawnResult);
-
-
-        return taskFinished;
+        return spawnResult == OK;
     },
 
 };
