b0254c19ad5838252278730545d0024551caf2bc
Davit Gharagebakyan <defghq@gmail.com>
2026-03-07 23:03:30 +0400

    asd

--- task.taxi.js
+++ task.taxi.js
@@ -29,7 +29,7 @@ module.exports = {
             return false;
         }
         else {
-            target.move(creep);
+            target.move(executer);
         }
 
         const executerAtDestination =
