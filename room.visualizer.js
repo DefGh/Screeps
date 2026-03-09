5471730dedf75adeb224f6ade1375cb0c37cd6a0
Davit Gharagebakyan <defghq@gmail.coom>
2026-03-09 21:25:53 +0400

    dsa

--- room.visualizer.js
+++ room.visualizer.js
@@ -102,6 +102,7 @@ module.exports = {
             }
             taskText += executorEmojis.join(' ');
         } else {
+            task.status = constants.taskStatuses.pending;
             taskText += 'нет исполнителей';
         }
         
