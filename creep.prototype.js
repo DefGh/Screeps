9b940c199c916c798b6ce6b39ab119b333528b41
Davit Gharagebakyan <defghq@gmail.coom>
2026-03-09 21:00:26 +0400

    dsa

--- creep.prototype.js
+++ creep.prototype.js
@@ -41,8 +41,6 @@ Creep.prototype.deliverEnergy = function(target) {
         return true;
     }
 
-    console.log(target.structureType);
-
     switch (target.structureType) {
         case STRUCTURE_SPAWN:
             if (this.transfer(target, RESOURCE_ENERGY) != OK) {
