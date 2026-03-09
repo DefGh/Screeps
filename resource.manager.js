b18bb25dcec46e6dc6d4b1211c0d0611fbd9feb2
Davit Gharagebakyan <defghq@gmail.coom>
2026-03-09 20:09:17 +0400

    das

--- resource.manager.js
+++ resource.manager.js
@@ -29,8 +29,7 @@ module.exports = {
     reservedTotal(source) {
         var amount = 0;
         for (let reservation in Memory.resourceManager.reservations) {
-            console.log(reservationId);
-            reservationId = Memory.resourceManager.reservations[reservationId];
+            let reservationId = Memory.resourceManager.reservations[reservationId];
             if (reservation.sourceId === source.id) {
                 amount += reservation.amount;
             }
